434d164df059e7cd9b308ef35bd5bcf450243322
Iana Glebova <letdrown@gmail.com>
2023-03-27 00:15:40 +0300

    Add Task4

---
+++ 4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int N;
+int N0 = 1;
+
+Console.Write("Введите число N: ");
+int.TryParse(Console.ReadLine(), out N);
+
+while(N0 < N)
+{
+Console.Write(N0); 
+Console.Write(", ");
+N0++;
+}
+Console.Write(N);
